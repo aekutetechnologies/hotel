d6e9ba687f79fef4e7311f12f887be3df26e2f2a
smruti <aekutetechnologies@gmail.com>
2025-04-05 18:17:42 +0530

    Updated the type issues

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,14 +8,16 @@ const nextConfig = {
         port: '8000',
         pathname: '/media/**',
       },
+      {
+        protocol: 'http',
+        hostname: '147.93.97.63',
+        pathname: '/media/**',
+      },
     ],
   },
   eslint: {
     // Warning: This allows builds to complete even if there are ESLint errors.
     ignoreDuringBuilds: true,
-  },
-  images: {
-    domains: ['147.93.97.63'],
   },
 };
 
